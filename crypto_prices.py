63ce331cb1a0b1ffb73f237253c70425c772e803
pavelexpertov <pavelm01@hotmail.co.uk>
2019-07-09 20:04:51 +0100

    add formatting and history command

--- crypto_prices.py
+++ crypto_prices.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import configparser
 
@@ -18,6 +19,13 @@ def get_todays_price(currency_code_from, currency_code_to):
     return response.json()[currency_code_to.upper()]
 
 
+def get_daily_price_history(currency_code_from, currency_code_to, number_of_days=10):
+    parameters = {'fsym': currency_code_from.upper(), 'tsym': currency_code_to.upper(), 'limit': number_of_days}
+    headers = {'authorization': 'Apikey ' + API_KEY}
+    response = requests.get(DATA_API_URL_TEMPLATE.format('histoday'), params=parameters, headers=headers)
+    return response.json()
+
+
 @click.group()
 def cli():
     '''A simple module to get prices for currencies cryptocurrencis as well as their history'''
@@ -28,10 +36,27 @@ def cli():
 @click.argument('currency_from')
 @click.argument('currency_to')
 def today(currency_from, currency_to):
+    '''Get today's price based on given currency codes.'''
     click.echo('Price: {0}'.format(get_todays_price(currency_from, currency_to)))
+
+@click.command()
+@click.argument('currency_from')
+@click.argument('currency_to')
+def history(currency_from, currency_to):
+    '''Get ten day history worth data based on given currency codes.'''
+    json_data = get_daily_price_history(currency_from, currency_to)
+    max_padding = max([len(str(day['close'])) for day in json_data['Data']])
+    template = "{0}: {1: " + str(max_padding) + ".2f}"
+    click.echo('template: ' + template)
+    date_format = '%d %b'
+    output_list = [template.format(datetime.fromtimestamp(day['time']).strftime(date_format), day['close'])
+                   for day in json_data['Data']]
+    click.echo('\n'.join(output_list))
+
 
 
 cli.add_command(today)
+cli.add_command(history)
 
 if __name__ == '__main__':
     cli()
