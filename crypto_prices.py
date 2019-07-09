064e99c79a04149966af11eefba3b890f6d06f2d
pavelexpertov <pavelm01@hotmail.co.uk>
2019-07-09 18:58:06 +0100

    implement todays function

--- crypto_prices.py
+++ crypto_prices.py
@@ -1,5 +1,21 @@
+import os
+import configparser
+
 import click
 import requests
+
+config_file_parser = configparser.ConfigParser()
+expanded_path = os.path.expanduser('~/.config/crypto-prices/config.ini')
+config_file_parser.read(expanded_path)
+API_KEY = config_file_parser['api']['key']
+DATA_API_URL_TEMPLATE = 'https://min-api.cryptocompare.com/data/{0}'
+
+def get_todays_price(currency_code_from, currency_code_to):
+    '''Return today's price based on a given currency code'''
+    parameters = {'fsym': currency_code_from.upper(), 'tsyms': currency_code_to.upper()}
+    headers = {'authorization': 'Apikey ' + API_KEY}
+    response = requests.get(DATA_API_URL_TEMPLATE.format('price'), params=parameters, headers=headers)
+    return response.json()[currency_code_to.upper()]
 
 
 @click.group()
@@ -12,8 +28,7 @@ def cli():
 @click.argument('currency_from')
 @click.argument('currency_to')
 def today(currency_from, currency_to):
-    click.echo('currency ' + currency_from)
-    click.echo('currency to ' + currency_to)
+    click.echo('Price: {0}'.format(get_todays_price(currency_from, currency_to)))
 
 
 cli.add_command(today)
